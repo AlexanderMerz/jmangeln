6cb78e3813b2242c4da691c2e44fa5349c6c4fd3
AlexanderMerz <merz.alexander@gmx.net>
2019-06-17 23:50:30 +0200

    Added compression

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const compression = require('compression');
 
 const app = express();
-
+app.use(compression());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('index.html'));
