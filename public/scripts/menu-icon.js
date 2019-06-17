e8c03d4ac48d94bac68bb5edf19726602306437c
AlexanderMerz <merz.alexander@gmx.net>
2019-06-17 23:55:00 +0200

    Refactoring

--- public/scripts/menu-icon.js
+++ public/scripts/menu-icon.js
@@ -1,14 +1,14 @@
 const navigation = document.querySelector('.navbar__navigation');
 const menuIcon = document.querySelector('.menu-icon');
-const listItems = document.querySelectorAll('.navbar__navigation ul li');
+const ul = document.querySelector('.navbar__navigation ul');
 
 menuIcon.addEventListener('click', function() {
     this.classList.toggle('change');
     if (this.classList.contains('change')) {
         navigation.style.setProperty('width', '100%');
-        listItems.forEach(li => li.style.setProperty('opacity', 1));
+        Array.from(ul.children).forEach(li => li.style.opacity =  1);
     } else {
         navigation.style.setProperty('width', '0');
-        listItems.forEach(li => li.style.setProperty('opacity', 0));
+        Array.from(ul.children).forEach(li => li.style.opacity =  0);
     }
 });
