451040ee148fd733c0d497a2f7e3d03c4b0fe642
AlexanderMerz <merz.alexander@gmx.net>
2019-06-22 01:44:12 +0200

    Added route /team

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const compression = require('compression');
+const path = require('path');
 
 const app = express();
 app.use(compression());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('index.html'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
+app.get('/team', (req, res) => res.sendFile(path.join(__dirname, 'public', 'pages', 'team.html')));
 
 app.listen(process.env.PORT || 8080);
