c364896e52242121934694f879afe2da9b3c57ae
AlexanderMerz <merz.alexander@gmx.net>
2019-07-17 16:49:11 +0200

    Added minor error handling

--- public/scripts/fetch.js
+++ public/scripts/fetch.js
@@ -3,15 +3,21 @@ window.addEventListener('load', async () => {
     const player = document.querySelector('.video-player iframe');
 
     if (!sessionStorage.getItem('youtube')) {
-        const response = await fetch('/api/youtube')
-        const json = await response.json();
-        sessionStorage.setItem('youtube', JSON.stringify(json));
+        try {
+            const response = await fetch('/api/youtube');
+            const json = await response.json();
+            if ('error' in json) return;
+            sessionStorage.setItem('youtube', JSON.stringify(json));
+        } catch(error) {
+            console.error(error);
+        }
     }
+
     const { items } = JSON.parse(sessionStorage.getItem('youtube'));
     player.src += items[0].id.videoId;
     for (let item of items) {
-        const video = document.createElement('player-item');
-        video.data = item;
-        playlist.appendChild(video);
+        const playlistItem = document.createElement('playlist-item');
+        playlistItem.data = item;
+        playlist.appendChild(playlistItem);
     }
 });
