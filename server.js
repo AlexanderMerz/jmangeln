9b70e2e43437db2886731f570596c7cfa66cc7fa
AlexanderMerz <merz.alexander@gmx.net>
2019-07-17 13:18:41 +0200

    Added routes including API endpoint

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
+const { getYoutubeData } = require('./youtube');
 
 const app = express();
 app.use(compression());
 app.use(express.static('public'));
 
-// path to pages folder
 const pages = path.join(__dirname, 'public', 'pages');
 
 app.get('/team', (req, res) => res.sendFile(pages + path.sep + 'team.html'));
 app.get('/social', (req, res) => res.sendFile(pages + path.sep + 'social.html'));
+app.get('/videos', (req, res) => res.sendFile(pages + path.sep + 'videos.html'));
+app.get('/merch', (req, res) => res.sendFile(pages + path.sep + 'merch.html'));
+app.get('/blog', (req, res) => res.sendFile(pages + path.sep + 'blog.html'));
+
+app.get('/api/youtube', async (req, res) => {
+    const data = await getYoutubeData();
+    return res.json(data);
+});
 
 app.listen(process.env.PORT || 8080);
