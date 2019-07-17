08b34dd5d4e9b832a90b0f9b5f8289223bbad899
AlexanderMerz <merz.alexander@gmx.net>
2019-07-17 16:09:17 +0200

    Removed alert and restructured code

--- public/scripts/fetch.js
+++ public/scripts/fetch.js
@@ -1,17 +1,17 @@
 window.addEventListener('load', async () => {
+    const playlist = document.querySelector('.playlist');
+    const player = document.querySelector('.video-player iframe');
+
     if (!sessionStorage.getItem('youtube')) {
         const response = await fetch('/api/youtube')
         const json = await response.json();
         sessionStorage.setItem('youtube', JSON.stringify(json));
-        alert('Fetch hat funktioniert');
     }
     const { items } = JSON.parse(sessionStorage.getItem('youtube'));
-    const playlist = document.querySelector('.playlist');
-    const player = document.querySelector('.video-player iframe');
     player.src += items[0].id.videoId;
     for (let item of items) {
-        const playerItem = document.createElement('player-item');
-        playerItem.data = item;
-        playlist.appendChild(playerItem);
+        const video = document.createElement('player-item');
+        video.data = item;
+        playlist.appendChild(video);
     }
 });
