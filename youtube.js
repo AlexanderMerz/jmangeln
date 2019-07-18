0659b77f6d02b272c0959262b00eb5df0442c5bd
AlexanderMerz <merz.alexander@gmx.net>
2019-07-18 14:06:17 +0200

    Changed env variable name

--- youtube.js
+++ youtube.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='
     + 'UCMq-1kxw85KCwgmiwuuP4iQ&maxResults=10&order=date&type=video&key='
-    + `${process.env.youtubeKey}`;
+    + `${process.env.YOUTUBE_KEY}`;
 
 module.exports = { 
     getYoutubeData: async function() {
