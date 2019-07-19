6236b49d33f948bb7ef9c1bc3fd45fe61d384ab6
AlexanderMerz <merz.alexander@gmx.net>
2019-07-19 14:15:09 +0200

    Removed root attribute

--- public/components/blog-post.js
+++ public/components/blog-post.js
@@ -1,13 +1,13 @@
 class BlogPost extends HTMLElement {
     constructor() {
         super();
-        this.root = this.attachShadow({ mode: 'open' });
+        this.attachShadow({ mode: 'open' });
     }
     set content(data) {
         const { title, content, image } = data;
         const date = new Date(data.date).toLocaleDateString();
 
-        this.root.innerHTML = `
+        this.shadowRoot.innerHTML = `
             <style>@import "../styles/blog-post.css"</style>
             <div class="post">
                 <h1>${title} vom ${date}</h1>
