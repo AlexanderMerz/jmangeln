d792510ab7995f5593b31ef782cdc6991b95ca3e
AlexanderMerz <merz.alexander@gmx.net>
2019-07-19 14:18:30 +0200

    Added lazy loading

--- public/components/playlist-item.js
+++ public/components/playlist-item.js
@@ -1,7 +1,9 @@
+import { loadLazy } from '../scripts/lazy-load.mjs';
+
 class PlaylistItem extends HTMLElement {
     constructor() {
         super();
-        this.root = this.attachShadow({ mode: 'open' });
+        this.attachShadow({ mode: 'open' });
     }
     set data(data) {
         const { videoId } = data.id;
@@ -9,16 +11,23 @@ class PlaylistItem extends HTMLElement {
         const date = new Date(publishedAt).toLocaleDateString();
         const imageHref = data.snippet.thumbnails.medium.url;
 
-        this.root.innerHTML = `
+        this.shadowRoot.innerHTML = `
             <style>@import "../styles/playlist-item.css"</style>
             <div class="video" data-video-id="${videoId}">
                 <div class="info">
                     <h3 class="info__title">${title}</h3>
                     <p class="info__date">${date}</p>
                 </div>
-                <img class="video__image" src="${imageHref}">
+                <img
+                    class="video__image"
+                    data-src="${imageHref}"
+                    alt="playlist-thumbnail"
+                >
             </div>
         `;
+    }
+    connectedCallback() {
+        loadLazy(this.shadowRoot.querySelector('img'));
     }
 }
 customElements.define('playlist-item', PlaylistItem);
