ca69316a2b18f448e33236e2000f9b55fdfc63d9
AlexanderMerz <merz.alexander@gmx.net>
2019-07-26 13:42:05 +0200

    Removed custom prompt

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -1,29 +1,5 @@
-const btn = document.querySelector('.pwa-btn');
-let deferredPrompt;
-
-btn.addEventListener('click', event => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then(choice => {
-        if (choice.outcome === 'accepted') {
-            console.log('Accepted');
-        }
-        btn.style.display = 'none';
-        deferredPrompt = null;
-    });
-});
-
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
         .then(res => console.log('Registered ', res))
         .catch(error => console.log(error));
 }
-
-window.addEventListener('beforeinstallprompt', event => {
-    event.preventDefault();
-    deferredPrompt = event;
-    btn.style.display = 'block';
-});
-
-window.addEventListener('appinstalled', event => {
-    console.log('App has been installed');
-})
