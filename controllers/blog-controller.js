e382edd5f3d9d49157126f9930cd202652ac9d73
AlexanderMerz <merz.alexander@gmx.net>
2019-07-28 15:01:15 +0200

    Refactoring

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -1,19 +1,13 @@
 const Blog = require('../models/Blog');
-const path = require('path');
+const fs = require('fs');
 
 exports.getBlogs = async (req, res, next) => {
   const blogs = await Blog.find().sort({ date: -1 });
+  blogs.map(blog => blog.content = fs.readFileSync(blog.content));
   res.setHeader('Content-Type', 'application/json');
   return res.status(200).json(blogs);
 };
 
-exports.postBlog = async (req, res, next) => {
-  const { title, content } = req.body;
-  const imageUrl = path.sep + req.file.path;
-  if (!title || !content || !imageUrl) res.redirect('/');
-  const blog = new Blog({ title, content, image });
-  blog.save(err => {
-    if (err) return res.redirect('/');
-    else return res.render('success');
-  });
+exports.postBlog = async (title, content, image) => {
+  new Blog({ title, content, image }).save();
 };
