617740f83365a87111b47686d1eec44f932b1066
AlexanderMerz <merz.alexander@gmx.net>
2019-07-28 15:03:41 +0200

    Discarded session storage strategy

--- public/scripts/fetch.js
+++ public/scripts/fetch.js
@@ -5,37 +5,30 @@ window.addEventListener('load', async () => {
         case '/videos': {
             const playlist = document.querySelector('.playlist');
             const player = document.querySelector('.video-player iframe');
-            if (!sessionStorage.getItem('youtube')) {
-                try {
-                    const response = await fetch('/api/youtube');
-                    const json = await response.json();
-                    if (json.status === 423) {
-                        player.src += 'YnjdFKQAZhw';
-                        return;
-                    }
-                    sessionStorage.setItem('youtube', JSON.stringify(json));
-                } catch(error) {
-                    console.error(error);
+            try {
+                const response = await fetch('/api/youtube');
+                const json = await response.json();
+                if (json.status === 423) {
+                    player.src += 'YnjdFKQAZhw';
+                    return;
                 }
-            }
-            const { data } = JSON.parse(sessionStorage.getItem('youtube'));
-            player.src += data.items[0].id.videoId;
-            for (let item of data.items) {
-                const playlistItem = document.createElement('playlist-item');
-                playlistItem.data = item;
-                playlist.appendChild(playlistItem);
+                const { data } = json;
+                player.src += data.items[0].id.videoId;
+                for (let item of data.items) {
+                    const playlistItem = document.createElement('playlist-item');
+                    playlistItem.data = item;
+                    playlist.appendChild(playlistItem);
+                }
+            } catch(error) {
+                console.error(error);
             }
             break;
         }
 
         case '/blog': {
             const blogSection = document.querySelector('.blog');
-            if (!sessionStorage.getItem('posts')) {
-                const response = await fetch('/api/blogs');
-                const json = await response.json();
-                sessionStorage.setItem('posts', JSON.stringify(json));
-            }
-            const posts = JSON.parse(sessionStorage.getItem('posts'));
+            const response = await fetch('/api/blogs');
+            const posts = await response.json();
             for (let post of posts) {
                 const blogPost = document.createElement('blog-post');
                 blogPost.content = post;
