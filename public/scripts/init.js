67b83f7c2060c07d65a46e01e8ba5bc7e0b9ba16
AlexanderMerz <merz.alexander@gmx.net>
2019-07-30 19:45:08 +0200

    Removed console logs

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -1,5 +1,3 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-        .then(res => console.log('Registered ', res))
-        .catch(error => console.log(error));
+    navigator.serviceWorker.register('./sw.js');
 }
