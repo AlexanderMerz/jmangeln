369b152e21b89b6acfc004abdf5daa537368220d
AlexanderMerz <merz.alexander@gmx.net>
2019-07-30 19:43:03 +0200

    Added method to retrieve post by ID

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -1,13 +1,19 @@
-const Blog = require('../models/Blog');
-const fs = require('fs');
+const { readFileSync } = require('fs');
+const BlogPost = require('../models/BlogPost');
 
-exports.getBlogs = async (req, res, next) => {
-  const blogs = await Blog.find().sort({ date: -1 });
-  blogs.map(blog => blog.content = fs.readFileSync(blog.content));
-  res.setHeader('Content-Type', 'application/json');
-  return res.status(200).json(blogs);
+exports.getPosts = async () => {
+    let posts = await BlogPost.find().sort({ date: -1 });
+    return posts;
+}
+
+exports.getPostById = async id => {
+    const post = await BlogPost.find({ id });
+    post.content = readFileSync(post.content);
+    return post;
+}
+
+exports.createPost = async (title, content, image) => {
+    const id = Math.round(Math.random() * 100);
+    new BlogPost({ id, title, content, image }).save();
 };
 
-exports.postBlog = async (title, content, image) => {
-  new Blog({ title, content, image }).save();
-};
