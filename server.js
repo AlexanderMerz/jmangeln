a1dcb99b9808c4ff3a568b22114903ad5fcd7138
AlexanderMerz <merz.alexander@gmx.net>
2019-08-01 20:10:22 +0200

    Outsourced blog related logic to blog controller

--- server.js
+++ server.js
@@ -36,11 +36,13 @@ const upload = multer({
     storage: multer.diskStorage({
         destination: (req, file, cb) => cb(null, 'uploads'),
         filename: (req, file, cb) => cb(
-            null, `${new Date().toISOString().split('T')[0]}`
+            null, `${new Date().getTime()}`
             + `.${file.mimetype.split('/')[1]}`
         )
     })
 });
+
+// new Date().toISOString().split('T')[0]
 
 /* Path to Pages Folder */
 const pages = path.join(__dirname, 'public', 'pages');
@@ -50,23 +52,12 @@ app.get('/team', (req, res) => res.sendFile(pages + path.sep + 'team.html'));
 app.get('/social', (req, res) => res.sendFile(pages + path.sep + 'social.html'));
 app.get('/videos', (req, res) => res.sendFile(pages + path.sep + 'videos.html'));
 app.get('/merch', (req, res) => res.sendFile(pages + path.sep + 'merch.html'));
-app.get('/blog/post/:id', async (req, res) => {
-    const post = await blogController.getPostById(req.params.id);
-    return res.json(post);
-});
+app.get('/blog/post/:id', blogController.getPostById);
 app.get('/blog', (req, res) => res.sendFile(pages + path.sep + 'blog.html'));
 
 /* Create Blog Posts */
 app.get('/create-post', (req, res) => res.sendFile(pages + path.sep + 'create-post.html'));
-app.post('/create-post', upload.single('image'), (req, res) => {
-    const { title, content } = req.body;
-    const filename = req.file.filename.split('.')[0];
-    const contentPath = path.join('uploads', `${filename}.txt`);
-    fs.writeFile(contentPath, content, err => {
-        if (!err) blogController.createPost(title, contentPath, req.file.path);
-    })
-    return res.redirect('/');
-});
+app.post('/create-post', upload.single('image'), blogController.createPost);
 
 /* API Endpoints */
 app.get('/api/youtube', async (req, res) => {
@@ -74,10 +65,7 @@ app.get('/api/youtube', async (req, res) => {
     if ('error' in data) return res.json({ status: 423, data });
     else return res.json({ status: 200, data });
 });
-app.get('/api/blogs', async (req, res) => {
-    const posts = await blogController.getPosts();
-    return res.json(posts);
-});
+app.get('/api/blogs', blogController.getPosts);
 
 /* Database Connection + Server Start */
 mongoose
