5747d101876affd9855be3d7716842cd6bcce51c
AlexanderMerz <merz.alexander@gmx.net>
2019-08-01 20:11:07 +0200

    Refactored exported functions

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -1,19 +1,35 @@
 const { readFileSync } = require('fs');
 const BlogPost = require('../models/BlogPost');
+const path = require('path');
+const fs = require('fs');
 
-exports.getPosts = async () => {
-    let posts = await BlogPost.find().sort({ date: -1 });
-    return posts;
+exports.getPosts = async (req, res) => {
+    const posts = await BlogPost.find().sort({ date: -1 });
+    return res.json(posts);
 }
 
-exports.getPostById = async id => {
-    const post = await BlogPost.find({ id });
+exports.getPostById = async (req, res) => {
+    const post = await BlogPost.findOne({ id: req.params.id });
     post.content = readFileSync(post.content);
-    return post;
+    return res.render('post', { post });
 }
 
-exports.createPost = async (title, content, image) => {
-    const id = Math.round(Math.random() * 100);
-    new BlogPost({ id, title, content, image }).save();
+exports.createPost = async (req, res) => {
+    const imageName = req.file.filename.split('.')[0];
+    const contentPath = path.join('uploads', `${imageName}.txt`);
+    try {
+        fs.writeFile(contentPath, req.body.content, error => {
+            if (error) throw error;
+            new BlogPost({ 
+                id: Math.round(Math.random() * 100), 
+                title: req.body.title, 
+                content: contentPath, 
+                image: req.file.path
+            }).save();
+        });
+    } catch (error) {
+        console.log(error);
+        return res.redirect('/');
+    }
+    return res.redirect('/');
 };
-
