85ef6fae78d21c9e321083f930bf562b393cfe69
AlexanderMerz <merz.alexander@gmx.net>
2019-08-01 20:12:47 +0200

    Disabled touchmove events

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -3,6 +3,8 @@ const navigation = document.querySelector('.navbar__navigation');
 const ul = document.querySelector('.navbar__navigation ul');
 const menuIcon = document.querySelector('.menu-icon');
 
+document.addEventListener('touchmove', event => event.preventDefault());
+
 logo.addEventListener('click', () => window.location = '/');
 
 menuIcon.addEventListener('click', function() {
