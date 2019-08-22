42cab20f2c94c6b4b9c01c007b0daebcb32c214b
AlexanderMerz <merz.alexander@gmx.net>
2019-08-22 11:23:36 +0200

    Disabled fetch.js temporarily

--- public/scripts/fetch.js
+++ public/scripts/fetch.js
@@ -1,40 +1,40 @@
-window.addEventListener('load', async () => {
+// window.addEventListener('load', async () => {
 
-    switch(this.location.pathname) {
+//     switch(this.location.pathname) {
 
-        case '/videos': {
-            const playlist = document.querySelector('.playlist');
-            const player = document.querySelector('.video-player iframe');
-            try {
-                const response = await fetch('/api/youtube');
-                const json = await response.json();
-                player.src += json.items[0].id.videoId;
-                for (let item of json.items) {
-                    const playlistItem = document.createElement('playlist-item');
-                    playlistItem.data = item;
-                    playlist.appendChild(playlistItem);
-                }
-            } catch(error) {
-                // Show fallback video
-                player.src += 'YnjdFKQAZhw';
-            }
-            break;
-        }
+//         case '/videos': {
+//             const playlist = document.querySelector('.playlist');
+//             const player = document.querySelector('.video-player iframe');
+//             try {
+//                 const response = await fetch('/api/youtube');
+//                 const json = await response.json();
+//                 player.src += json.items[0].id.videoId;
+//                 for (let item of json.items) {
+//                     const playlistItem = document.createElement('playlist-item');
+//                     playlistItem.data = item;
+//                     playlist.appendChild(playlistItem);
+//                 }
+//             } catch(error) {
+//                 // Show fallback video
+//                 player.src += 'YnjdFKQAZhw';
+//             }
+//             break;
+//         }
 
-        case '/blog': {
-            const blogSection = document.querySelector('.blog');
-            const response = await fetch('/api/blogs');
-            const posts = await response.json();
-            for (let post of posts) {
-                const blogPost = document.createElement('blog-post');
-                blogPost.content = post;
-                blogSection.appendChild(blogPost);
-            }
-            break;
-        }
+//         case '/blog': {
+//             const blogSection = document.querySelector('.blog');
+//             const response = await fetch('/api/blogs');
+//             const posts = await response.json();
+//             for (let post of posts) {
+//                 const blogPost = document.createElement('blog-post');
+//                 blogPost.content = post;
+//                 blogSection.appendChild(blogPost);
+//             }
+//             break;
+//         }
 
-        default: return;
+//         default: return;
         
-    }
+//     }
 
-});
+// });
