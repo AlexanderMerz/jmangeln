0fd72cf4daf509c500c49546a655cd3d1db8b335
AlexanderMerz <merz.alexander@gmx.net>
2019-09-01 16:31:17 +0200

    Added caching strategy for products

--- public/sw.js
+++ public/sw.js
@@ -16,18 +16,13 @@ const assets = [
     '/scripts/lazy-load.mjs',
     '/components/info-card.js',
     '/images/brand.webp',
-    '/images/brand.jpg',
+    '/images/brand.jpg'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(staticCache).then(cache => {
-            assets.forEach(asset => cache.add(asset).catch(error => {
-                console.log('Error: ', error);
-                console.log('Asset: ', asset);
-            }));
-        })
-    );
+    event.waitUntil(caches.open(staticCache).then(cache => {
+        assets.forEach(asset => cache.add(asset));
+    }).catch(error => console.log(error)));
 });
 
 self.addEventListener('activate', event => {
@@ -42,21 +37,16 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-    if (event.request.url.indexOf('/api/blog') > 0) {
-        event.respondWith(
-            fetch(event.request.url).then(fetchRes => {
+    if (event.request.url.indexOf('/api/products') > 0) {
+        caches.match(event.request).then(cacheRes => {
+            if (cacheRes) return cacheRes;
+            else return fetch(event.request).then(fetchRes => {
                 return caches.open(dynamicCache).then(cache => {
                     cache.put(event.request.url, fetchRes.clone());
                     return fetchRes;
-                })
-            }).catch(() => {
-                caches.match(event.request).then(response => {
-                    if (!response) return caches.open(staticCache).then(cache => {
-                        return cache.match('/pages/fallback.html');
-                    })
-                })
-            })
-        );
+                });
+            });
+        })
     } else if (event.request.url.indexOf('/api/youtube') > 0) {
         event.respondWith(
             caches.match(event.request).then(cacheRes => {
@@ -65,7 +55,6 @@ self.addEventListener('fetch', event => {
                 if (cacheRes && cacheRes.status !== 400) {
                     const cachedAt = new Date(cacheRes.headers.get('date')).getTime();
                     const now = new Date().getTime();
-                    console.log(now - cachedAt);
                     if (( now - cachedAt ) < 8640000) return cacheRes;
                 }
                 // If response is null or time difference is greater than 24 hours
@@ -80,17 +69,31 @@ self.addEventListener('fetch', event => {
     } else {
         event.respondWith(
             caches.match(event.request).then(cacheRes => {
-                return cacheRes || fetch(event.request).then(fetchRes => {
-                    return caches.open(dynamicCache).then(cache => {
-                        cache.put(event.request.url, fetchRes.clone());
-                        return fetchRes;
-                    });
-                }).catch(() => {
-                    return caches.open(staticCache).then(cache => {
-                        return cache.match('/pages/fallback.html')
-                    });
+                return cacheRes || fetch(event.request).then(async fetchRes => {
+                    const cache = await caches.open(dynamicCache);
+                    cache.put(event.request.url, fetchRes.clone());
+                    return fetchRes;
+                }).catch(async () => {
+                    const cache = await caches.open(staticCache);
+                    return cache.match('/pages/fallback.html');
                 });
             })
         );
     }
 });
+
+// Network first, then cache strategy
+// event.respondWith(
+//     fetch(event.request.url).then(async fetchRes => {
+//         const cache = await caches.open(dynamicCache);
+//         cache.put(event.request.url, fetchRes.clone());
+//         return fetchRes;
+//     }).catch(() => {
+//         caches.match(event.request).then(async response => {
+//             if (!response) {
+//                 const cache = await caches.open(staticCache);
+//                 return cache.match('/pages/fallback.html');
+//             }
+//         })
+//     })
+// );
