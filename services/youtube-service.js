daa145bead2f846b34bf4ada95bebdd388039b11
AlexanderMerz <merz.alexander@gmx.net>
2019-09-10 11:19:07 +0200

    Moved video fetch into youtube-service file

---
+++ services/youtube-service.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+const baseUrl = 'https://www.googleapis.com/youtube/v3/search';
+const apiKey = process.env.YOUTUBE_KEY;
+const channelID = 'UCMq-1kxw85KCwgmiwuuP4iQ'
+
+exports.fetchVideos = async () => {
+    const url = baseUrl + `?part=snippet&channelId=${channelID}`
+        + `&maxResults=10&order=date&type=video&key=${apiKey}`;
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        throw error;
+    }
+};
