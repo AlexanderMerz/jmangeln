7aa6d388463a99e638bd6683662fe4d3f5eeedd2
AlexanderMerz <merz.alexander@gmx.net>
2019-09-10 11:18:54 +0200

    Added heading for the list containing meta data

--- public/scripts/fetch.js
+++ public/scripts/fetch.js
@@ -12,8 +12,11 @@ window.addEventListener('load', async () => {
                 productCard.setAttribute('name', product.name);
                 productCard.setAttribute('price', product.price);
                 productCard.setAttribute('image', product.image);
+                productCard.dataset.id = product._id;
                 const meta = product.meta.map(data => `<li>${data}</li>`);
-                productCard.innerHTML = `<ul>${meta.join('')}</ul>`;
+                productCard.innerHTML = product.meta.some(data => data.length > 2)
+                    ? '<h3>Farbe wählen</h3>' : '<h3>Größe wählen</h3>';
+                productCard.innerHTML += `<ul>${meta.join('')}</ul>`;
                 wrapper.appendChild(productCard);
             }
         }
