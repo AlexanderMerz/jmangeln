8ae84b7eb2ae0974e23cce43716abdd24a6f553f
AlexanderMerz <merz.alexander@gmx.net>
2019-09-19 20:22:24 +0200

    Commented out product fetch

--- public/scripts/fetch.js
+++ public/scripts/fetch.js
@@ -3,28 +3,28 @@ window.addEventListener('load', async () => {
     switch(this.location.pathname) {
 
         case '/merch': {
-            const response = await fetch('/api/products');
-            const products = await response.json();
-            console.log(products);
-            const wrapper = document.querySelector('.card-wrapper');
-            for (let product of products) {
-                const productCard = document.createElement('product-card');
-                productCard.setAttribute('name', product.name);
-                productCard.setAttribute('price', product.price);
-                productCard.setAttribute('image', product.image);
-                productCard.dataset.id = product._id;
-                const options = product.meta.map(data => 
-                    `<option value="${data}">${data}</option>`);
-                productCard.innerHTML = product.meta
-                    .some(data => data.length > 2)
-                        ? '<h3>Farbe</h3>'
-                        : '<h3>Größe</h3>';
-                productCard.innerHTML += `
-                    <div class="select-wrapper">
-                        <select>${options}</select>
-                    </div>`;
-                wrapper.appendChild(productCard);
-            }
+            const response = await fetch('/api/categories');
+            const categories = await response.json();
+            console.log(categories);
+            // const wrapper = document.querySelector('.card-wrapper');
+            // for (let product of products) {
+            //     const productCard = document.createElement('product-card');
+            //     productCard.setAttribute('name', product.name);
+            //     productCard.setAttribute('price', product.price);
+            //     productCard.setAttribute('image', product.image);
+            //     productCard.dataset.id = product._id;
+            //     const options = product.meta.map(data => 
+            //         `<option value="${data}">${data}</option>`);
+            //     productCard.innerHTML = product.meta
+            //         .some(data => data.length > 2)
+            //             ? '<h3>Farbe</h3>'
+            //             : '<h3>Größe</h3>';
+            //     productCard.innerHTML += `
+            //         <div class="select-wrapper">
+            //             <select>${options}</select>
+            //         </div>`;
+            //     wrapper.appendChild(productCard);
+            // }
         }
 
         case '/blog': {
