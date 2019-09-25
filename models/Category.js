5e9b66ffcbc9c8459a5f029b25a5a16f1f8e39d6
AlexanderMerz <merz.alexander@gmx.net>
2019-09-25 13:47:16 +0200

    Added _id property

--- models/Category.js
+++ models/Category.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     name: String,
-    image: String
+    image: String,
+    href: String
 });
 
 module.exports = mongoose.model('Category', categorySchema);
