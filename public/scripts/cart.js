03f52031c3bca9b22749037b2c4fdabeaff146de
AlexanderMerz <merz.alexander@gmx.net>
2019-10-12 12:43:08 +0200

    Slight changes to navbar

--- public/scripts/cart.js
+++
@@ -1,6 +0,0 @@
-// const cartIcon = document.querySelector('.cart img');
-// cartIcon.addEventListener('click', function() {
-//     if (this.parentElement.offsetWidth > 70) {
-//         document.activeElement = null;
-//     }
-// });
