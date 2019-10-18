1c7a0ab6ed5c59596938638181176b4a5ab43418
AlexanderMerz <merz.alexander@gmx.net>
2019-10-18 13:07:16 +0200

    Changed animation of side navigation

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -8,10 +8,10 @@ logo.addEventListener('click', () => window.location = '/');
 menuIcon.addEventListener('click', function() {
     this.classList.toggle('change');
     if (this.classList.contains('change')) {
-        navigation.style.setProperty('width', '100%');
+        navigation.style.setProperty('height', '100%');
         Array.from(ul.children).forEach(li => li.style.opacity =  1);
     } else {
-        navigation.style.setProperty('width', '0');
+        navigation.style.setProperty('height', '0');
         Array.from(ul.children).forEach(li => li.style.opacity =  0);
     }
 });
