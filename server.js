3956caa1a186e6364acb0a4dab65cd8bba668b6f
AlexanderMerz <merz.alexander@gmx.net>
2019-10-19 16:47:00 +0200

    Added maxAge of cookie. Modified merch routes

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
+    cookie: { maxAge: 1000* 60 * 60 * 24 * 7 },
     saveUninitialized: false,
     secret: 'my-secret',
     resave: false,
@@ -74,18 +75,30 @@ app.get('/social', (req, res) => res.sendFile(pages + path.sep + 'social.html'))
 app.get('/videos', (req, res) => res.sendFile(pages + path.sep + 'videos.html'));
 
 /* Merch & Merch Subpages */
-app.get('/merch/*', async (req, res) => {
-    const endpoint = req.url.split('merch/')[1];
-    if (endpoint === 'cart') {
-        return res.render('cart');
+app.get('/merch*', async (req, res) => {
+
+    const cart = cartController.getCart(req);
+    const quantity = cartController.getQuantity(cart);
+    const total = cartController.getTotal(cart);
+
+    switch (req.url) {
+        case '/merch': res.render('merch', { quantity }); break;
+        case '/merch/cart': res.render('cart', { cart, quantity, total }); break;
+        default: 
+            const category = capitalizeFirstLetter(req.url.split('/merch/')[1]);
+            const products = await productController
+                .findProductsByCategory(category.toLowerCase());
+            return products.length > 0
+                ? res.status(200).render('product-list', {
+                    category, products, quantity
+                }) : res.status(404).redirect('/');
     }
-    const category = capitalizeFirstLetter(endpoint);
-    const products = await productController.findProductsByCategory(category.toLowerCase());
-    return products.length > 0
-        ? res.status(200).render('product-list', { category, products })
-        : res.status(404).redirect('/');
+
 });
-app.get('/merch', (req, res) => res.sendFile(pages + path.sep + 'merch.html'));
+
+app.post('/merch/cart', parseText, cartController.postCart);
+
+// app.get('/merch', (req, res) => res.sendFile(pages + path.sep + 'merch.html'));
 app.get('/produkt/:id', async (req, res) => {
     const product = await productController.findProductById(req.params.id);
     return product
@@ -113,12 +126,7 @@ app.get('/api/blogs', blogController.getPosts);
 app.get('/api/products', productController.getProducts);
 app.get('/api/categories', categoryController.getCategories);
 
-/* Shopping Cart */
-app.post('/cart', parseText, cartController.postCart);
-app.get('/cart', cartController.getCart);
-
 app.get('/cookie', (req, res) => {
-    console.log(req.session.isLoggedIn);
     return res.end('Cookie!');
 });
 
