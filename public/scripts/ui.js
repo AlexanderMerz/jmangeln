88a79bc575e2d9d95484f86677126931ac52d270
AlexanderMerz <merz.alexander@gmx.net>
2019-11-26 09:43:11 +0100

    Added function setOpacity

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -1,23 +1,28 @@
-const logo = document.querySelector('.navbar__brand img');
-const navigation = document.querySelector('.navbar__navigation');
-const ul = document.querySelector('.navbar__navigation ul');
-const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('header');
+const brand = navbar.querySelector('.navbar__brand');
+const navigation = navbar.querySelector('nav');
+const ul = navigation.querySelector('ul');
+const menuIcon = navbar.querySelector('.menu-icon');
 
-logo.addEventListener('click', () => {
- window.location = '/'; 
+brand.addEventListener('click', function() {
+    window.location = '/';
 });
+
+function setOpacity(value) {
+    return function(element) {
+        if (element instanceof HTMLElement) {
+            element.style.setProperty('opacity', value);
+        }
+    };
+}
 
 menuIcon.addEventListener('click', function () {
     this.classList.toggle('change');
     if (this.classList.contains('change')) {
         navigation.style.setProperty('height', '100%');
-        Array.from(ul.children).forEach(li => {
- li.style.opacity =  1; 
-});
+        Array.from(ul.children).forEach(setOpacity(1));
     } else {
         navigation.style.setProperty('height', '0');
-        Array.from(ul.children).forEach(li => {
- li.style.opacity =  0; 
-});
+        Array.from(ul.children).forEach(setOpacity(0));
     }
 });
